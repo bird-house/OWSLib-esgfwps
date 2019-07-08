3c57a30617768f55a6132f0702ef7df2f502c686
Pingu Carsti <ehbrecht@dkrz.de>
2019-07-08 17:16:22 +0200

    use package folder with __version__.py

---
+++ owslib_esgfwps/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Emu."""
+
+from .__version__ import __author__, __email__, __version__
+
+from .cwt import (
+    ParameterError,
+    Output,
+    Outputs,
+    Variable,
+    Variables,
+    Dimension,
+    Domain,
+    Domains,
+    Operation,
+    Operations
+)
