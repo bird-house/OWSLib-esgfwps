7bf1baee4552f88f9a2887c02cf2ded6f3f289c9
Pingu Carsti <ehbrecht@dkrz.de>
2019-07-08 17:29:13 +0200

    fixed pelican test

--- tests/test_wps_pelican.py
+++ tests/test_wps_pelican.py
@@ -1,23 +1,25 @@
-"""Tests with WPS `Emu` service."""
+"""Tests with WPS `Pelican` service:
+https://github.com/bird-house/pelican
+"""
 
 import pytest
 
 from owslib.wps import WebProcessingService, SYNC
-from owslib_esgfwps import Domain, Dimension, Variable
+from owslib_esgfwps import Domain, Domains, Dimension, Variable, Variables
 
 from .common import TEST_SU_OPENDAP
 
 
 @pytest.mark.online
-def test_wps_emu_subset():
-    headers = {'api_key': 'TOKEN'}
+def test_wps_pelican_subset():
+    headers = {'COMPUTE-TOKEN': 'TOKEN'}
     wps = WebProcessingService(
         url='http://localhost:5000/wps',
         headers=headers, verify=True)
-    d0 = Domain([Dimension('time', 0, 1, crs='indices')])
+    d0 = Domain(dict(time=Dimension(0, 1, crs='indices')))
     v0 = Variable(uri=TEST_SU_OPENDAP, var_name='su')
     exec = wps.execute(
-        'emu_subset',
-        inputs=[('domain', d0), ('variable', v0)],
+        'pelican_subset',
+        inputs=[('domain', Domains([d0])), ('variable', Variables([v0]))],
         mode=SYNC)
     assert exec.isSucceded()
